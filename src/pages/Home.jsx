1c3d002eb354995839ab82c96139ebd4320c64aa
Matteo Gassend <mgassend@gmail.com>
2019-08-27 13:32:07 +0200

    added error message if input field is empty

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,29 @@
 import React, {useState} from 'react'
 import actions from '../store/actions'
 import { connect } from 'react-redux';
-import { MDBContainer, MDBRow, MDBInput, MDBCol, MDBBtn } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBInput, MDBCol, MDBBtn, MDBAlert } from 'mdbreact';
 
 
 const Home = ({updateMagnet, history}) => {
     const [magnet, changeMagnet] = useState("")
+    const [alert, updateAlert] = useState(false)
     return (
         <MDBContainer>
         <h2 className="text-center">TorrVision</h2>
+        {alert && <MDBAlert color="danger">Please provide a magnet link or info hash</MDBAlert>}
         <MDBRow center className="mt-5 pt-5">
             <MDBCol md="8">
                 <MDBInput getValue={(value) => changeMagnet(value)} label="Magnet or Info Hash here" />
             </MDBCol>
             <MDBCol md="4">
                 <MDBBtn color="primary" onClick={() => {
-                    updateMagnet(magnet)
-                    history.push("/view")
+                    if (magnet === "") {
+                        updateAlert(true)
+                    } else {
+                        updateAlert(false)
+                        updateMagnet(magnet)
+                        history.push("/view")
+                    }
                 }}>View That</MDBBtn>
             </MDBCol>
         </MDBRow>
