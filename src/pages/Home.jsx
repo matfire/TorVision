cc93de4749d1c763c21ee74792323dd0360e8865
unknown <mgassend@gmail.com>
2019-09-14 10:15:14 +0200

    upadte homepage to include a simple help message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,9 @@ import { MDBContainer, MDBRow, MDBInput, MDBCol, MDBBtn, MDBAlert } from 'mdbrea
 const Home = ({updateMagnet, history}) => {
     const [magnet, changeMagnet] = useState("")
     const [alert, updateAlert] = useState(false)
+
     return (
-        <MDBContainer>
+        <MDBContainer className="mb-5 pt-5">
         <h2 className="text-center">TorrVision</h2>
         {alert && <MDBAlert color="danger">Please provide a magnet link or info hash</MDBAlert>}
         <MDBRow center className="mt-5 pt-5">
@@ -25,6 +26,12 @@ const Home = ({updateMagnet, history}) => {
                         history.push("/view")
                     }
                 }}>View That</MDBBtn>
+            </MDBCol>
+        </MDBRow>
+        <MDBRow className="mt-5">
+            <MDBCol md="12">
+                <h3 className="text-center">Need help ?</h3>
+                <p className="mt-3 text-center">Simply copy and paste an Infohash or magnet link into the input field above this text and click on "view that"</p>
             </MDBCol>
         </MDBRow>
     </MDBContainer>
