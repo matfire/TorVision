25a4b1640f8c0340e0be701c27d736b95d6b6d70
Matteo Gassend <matteo.gassend@epitech.eu>
2020-09-04 17:52:23 +0200

    clean slate protocol

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import store from './store/store';
-import Routes from './components/Routes';
-import {MDBFooter, MDBContainer} from 'mdbreact'
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes />
-      </Router>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('content-wrap'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
